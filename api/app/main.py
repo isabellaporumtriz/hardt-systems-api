192de63f753937ace7649663340b9fd7c4546125
Isabella Souza <issouza038@gmail.com>
2026-08-13 07:13:54 -0300

    fix: allow portal domain in CORS

--- api/app/main.py
+++ api/app/main.py
@@ -44,6 +44,7 @@ app.add_middleware(
         "http://localhost:3000",
         "http://127.0.0.1:3000",
         "https://hardt-admin.vercel.app",
+        "https://portal.hardtsystems.com",
     ],
     allow_credentials=True,
     allow_methods=["*"],
